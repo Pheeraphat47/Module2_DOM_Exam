bb50e2cdf0a099d72b3a5884b0f0cecb22a72637
Pheeraphat47 <dpheeraphat2547@gmail.com>
2023-12-14 20:49:10 +0700

    last commit

--- guestForm.js
+++ guestForm.js
@@ -1,97 +1,118 @@
 //65130500053 Pheeraphat Dherachaisuphakij
-//import { createGuestList } from './data/guestdata.js'
+// import { createGuestList } from './data/guestdata.js'
 const createGuestList = require('./data/guestdata.js')
 
-const guestList = createGuestList()
+// Create an instance of the guest list using createGuestList
+const guestList = createGuestList();
+
+// Define a function called guestForm
 function guestForm() {
-  //provide initial guests data list created from GuestManagement class
-  let guests = guestList
+  // Provide initial guests data list created from GuestManagement class
+  let guests = guestList;
 
-  // 1. register event for searching and adding
+  // 1. Register event handling for searching and adding guests
   function registerEventHandling() {
-    const searchInput = document.querySelector('#search-input')
-    searchInput?.addEventListener('keyup', searchGuest)
+    // Find the DOM element for the search input
+    const searchInput = document.querySelector('#search-input');
+    // Add 'keyup' event listener for guest search
+    searchInput.addEventListener('keyup', searchGuest);
 
-    const addBtn = document.getElementById('add-guest-btn')
-    addBtn?.addEventListener('click', addGuest)
+    // Find the DOM element for the "Add Guest" button
+    const addBtn = document.getElementById('add-guest-btn');
+    // Add 'click' event listener for adding guests
+    addBtn.addEventListener('click', addGuest);
   }
 
   // 2. Function to display one guest in the display area
   function displayGuest(guestItem) {
-    const displayArea = document.getElementById('display-area')
+    // Find the DOM element of the display area
+    const displayArea = document.getElementById('display-area');
 
-    const divEl = document.createElement('div')
-    const spanE1 = document.createElement('span')
-    const spanE2 = document.createElement('span')
+    // Create HTML elements for displaying the guest
+    const divEl = document.createElement('div');
+    const spanE1 = document.createElement('span');
+    const spanE2 = document.createElement('span');
 
-    spanE1.textContent = `${guestItem.firstname} ${guestItem.lastname}`
+    // Set the content of the first span with the guest's full name
+    spanE1.textContent = `${guestItem.firstname} ${guestItem.lastname}`;
 
-    spanE2.setAttribute('id', `${guestItem.firstname}-${guestItem.lastname}`)
-    spanE2.setAttribute('class', 'remove-icon')
-    spanE2.setAttribute('style', 'cursor:pointer;color:red')
-    spanE2.textContent = '[X]'
+    // Set attributes for the second span (remove icon)
+    spanE2.setAttribute('id', `${guestItem.firstname}-${guestItem.lastname}`);
+    spanE2.setAttribute('class', 'remove-icon');
+    spanE2.setAttribute('style', 'cursor:pointer;color:red');
+    spanE2.textContent = '[X]';
 
-    displayArea.appendChild(divEl)
-    divEl.appendChild(spanE1)
-    divEl.appendChild(spanE2)
+    // Append elements to the display area
+    displayArea.appendChild(divEl);
+    divEl.appendChild(spanE1);
+    divEl.appendChild(spanE2);
 
-    spanE2.addEventListener('click', removeGuest)
+    // Add event listener for removing guests when the remove icon is clicked
+    spanE2.addEventListener('click', removeGuest);
   }
 
   // 3. Function to display all existing guests in the display area
   function displayGuests(guestResult) {
-    const displayArea = document.getElementById('display-area')
-    displayArea.textContent = ''
-    for (const i of guestResult) {
-      displayGuest(i)
-    }
+    // Find the DOM element of the display area
+    const displayArea = document.getElementById('display-area');
+    // Clear previous content in the display area
+    displayArea.textContent = '';
 
+    // Create elements for displaying guest data
+    for (const i of guestResult) {
+      displayGuest(i);
+    }
   }
 
   // 4. Function to search and display matching guests
   function searchGuest(event) {
-    displayGuests(guests.searchGuests(event.target.value))
-
+    // Search for guests based on the input value and display the results
+    displayGuests(guests.searchGuests(event.target.value));
   }
-
 
   // 5. Function to add a new guest
   function addGuest() {
-    const inputFirstname = document.getElementById('firstname-input')
-    const inputLastname = document.getElementById('lastname-input')
-    //console.log(inputFirstname.value);
-    if (inputFirstname.value !== null && inputLastname.value !== null) {
-      let newGuest = guests.addNewGuest(inputFirstname.value, inputLastname.value)
+    // Find DOM elements for the input of firstname and lastname
+    const inputFirstname = document.getElementById('firstname-input');
+    const inputLastname = document.getElementById('lastname-input');
 
+    // Add a new guest and display the result
+    if (inputFirstname.value !== null && inputLastname.value !== null) {
+      let newGuest = guests.addNewGuest(inputFirstname.value, inputLastname.value);
       displayGuest(newGuest[newGuest.length - 1]);
     }
-    inputFirstname.value = ''
-    inputLastname.value = ''
+
+    // Clear input values after adding a new guest
+    inputFirstname.value = '';
+    inputLastname.value = '';
   }
 
   // 6. Function to remove a guest
   function removeGuest(event) {
-    let guest = event.target.id
-    let StG = guest.split('-')
-    let rmguest = { firstname: StG[0], lastname: StG[1] }
-    guests.removeGuest(rmguest)
+    // Extract the guest's id from the clicked element
+    let guest = event.target.id;
+    let StG = guest.split('-');
+    let rmguest = { firstname: StG[0], lastname: StG[1] };
 
-    const displayArea = document.getElementById('display-area')
-    const parent = document.getElementById(event.target.id)
-    displayArea.removeChild(parent.parentElement)
+    // Remove the guest and update the display area
+    guests.removeGuest(rmguest);
+    const displayArea = document.getElementById('display-area');
+    const parent = document.getElementById(event.target.id);
+    displayArea.removeChild(parent.parentElement);
   }
 
+  // Return an object with functions that can be used externally
   return {
     registerEventHandling,
     displayGuests,
     searchGuest,
     addGuest,
-    removeGuest,    
-  }
+    removeGuest
+  };
 }
-
-module.exports = guestForm
-// export { guestForm }
+module.exports = guestForm  // for test case
+// export { guestForm } // browser
+// // ต้องเปิดไว้ไม่งั้นใน browser ไม่ขึ้น
 // const { registerEventHandling, displayGuests } = guestForm()
 // registerEventHandling()
 // displayGuests(guestList.getAllGuests())
